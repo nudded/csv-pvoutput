e38cd27cf7b76c20064534d03b5e11a32a060880
Toon Willems <willemstoon@gmail.com>
2017-01-01 19:26:31 +0000

    add some configuration

---
+++ src/parsing.rs
@@ -0,0 +1,44 @@
+use rustc_serialize::{Decoder, Decodable};
+use std::fmt;
+use std::str::FromStr;
+use chrono::{DateTime, Local, TimeZone, FixedOffset};
+
+#[derive(Debug)]
+pub struct FloatWithPoint(f64);
+
+#[derive(Debug)]
+pub struct MyDateTime(DateTime<Local>);
+
+impl fmt::Display for FloatWithPoint {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl fmt::Display for MyDateTime {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+#[derive(RustcDecodable)]
+pub struct PvOutputRecord {
+    pub datetime: MyDateTime,
+    pub value: FloatWithPoint,
+    pub othervalue: FloatWithPoint,
+}
+
+impl Decodable for FloatWithPoint {
+    fn decode<D: Decoder>(d: &mut D) -> Result<FloatWithPoint, D::Error> {
+        let str_val = d.read_str().unwrap_or(String::new()).replace(",", ".");
+        Ok(FloatWithPoint(f64::from_str(&*str_val).unwrap()))
+    }
+}
+
+impl Decodable for MyDateTime {
+    fn decode<D: Decoder>(d: &mut D) -> Result<MyDateTime, D::Error> {
+        let str_val = d.read_str().unwrap_or(String::new());
+        Ok(MyDateTime(Local::from_offset(&FixedOffset::east(7200)).datetime_from_str(&*str_val, "%d.%m.%Y %H:%M").unwrap()))
+    }
+}
+
